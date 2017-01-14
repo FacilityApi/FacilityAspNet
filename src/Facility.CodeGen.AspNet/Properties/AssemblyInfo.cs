047862614bd185dd333cd3056e3c426ab348776c
Ed Ball <ed.ball@logos.com>
2017-01-14 09:51:39 -0800

    Fix descriptions.

--- src/Facility.CodeGen.AspNet/Properties/AssemblyInfo.cs
+++ src/Facility.CodeGen.AspNet/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Facility.CodeGen.AspNet")]
-[assembly: AssemblyDescription("A tool that generates ASP.NET for a Facility Service Definition.")]
+[assembly: AssemblyDescription("Used by fsdgenaspnet to generate Markdown for a Facility Service Definition.")]
 [assembly: AssemblyProduct("Facility.CodeGen.AspNet")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
