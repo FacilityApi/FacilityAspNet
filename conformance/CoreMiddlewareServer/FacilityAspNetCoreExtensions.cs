1d922e85ec5e1a859a55d7f081f703adf6eee0f4
Ed Ball <ed.ball@faithlife.com>
2019-05-28 14:10:32 -0700

    Add CoreMiddlewareServer.

---
+++ conformance/CoreMiddlewareServer/FacilityAspNetCoreExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using Facility.Core.Http;
+using Microsoft.AspNetCore.Builder;
+
+namespace CoreMiddlewareServer
+{
+	/// <summary>
+	/// ASP.NET Core extension methods for Facility.
+	/// </summary>
+	public static class FacilityAspNetCoreExtensions
+	{
+		/// <summary>
+		/// Adds a Facility service HTTP handler to the pipeline.
+		/// </summary>
+		/// <remarks>The handler itself should be configured as a service.</remarks>
+		public static IApplicationBuilder UseFacilityHttpHandler<T>(this IApplicationBuilder builder)
+			where T : ServiceHttpHandler
+		{
+			if (builder == null)
+				throw new ArgumentNullException(nameof(builder));
+
+			return builder.UseMiddleware<FacilityAspNetCoreMiddleware<T>>();
+		}
+	}
+}
---
+++ conformance/CoreMiddlewareServer/FacilityAspNetCoreMiddleware.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Facility.Core.Http;
+using Microsoft.AspNetCore.Http;
+
+namespace CoreMiddlewareServer
+{
+	/// <summary>
+	/// ASP.NET Core middleware for a Facility service HTTP handler.
+	/// </summary>
+	/// <typeparam name="T">The type of the ServiceHttpHandler.</typeparam>
+	public sealed class FacilityAspNetCoreMiddleware<T>
+		where T : ServiceHttpHandler
+	{
+		/// <summary>
+		/// Creates an instance.
+		/// </summary>
+		public FacilityAspNetCoreMiddleware(RequestDelegate next, T handler)
+		{
+			m_next = next;
+			m_handler = handler;
+		}
+
+		/// <summary>
+		/// Invokes the middleware.
+		/// </summary>
+		public async Task Invoke(HttpContext httpContext)
+		{
+			var httpRequestMessage = FacilityAspNetCoreUtility.CreateHttpRequestMessage(httpContext.Request);
+
+			HttpResponseMessage httpResponseMessage;
+			try
+			{
+				httpResponseMessage = await m_handler.TryHandleHttpRequestAsync(httpRequestMessage, httpContext.RequestAborted).ConfigureAwait(false);
+			}
+			catch (Exception exception)
+			{
+				httpResponseMessage = FacilityAspNetCoreUtility.CreateHttpResponseMessage(exception);
+			}
+
+			if (httpResponseMessage != null)
+			{
+				using (httpResponseMessage)
+					await FacilityAspNetCoreUtility.WriteHttpResponseMessageAsync(httpResponseMessage, httpContext.Response);
+			}
+			else
+			{
+				await m_next(httpContext).ConfigureAwait(false);
+			}
+		}
+
+		readonly RequestDelegate m_next;
+		readonly T m_handler;
+	}
+}
