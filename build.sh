767bfa0830dae67409799fff9595548f9709a457
Ed Ball <ed.ball@faithlife.com>
2020-10-11 17:53:25 -0700

    Retry Linux build with MONO_ROOT.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+export MONO_ROOT=$(dirname $(which mono))/../
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 dotnet publish ./tools/Build/Build.csproj --output ./tools/bin/Build --nologo --verbosity quiet
 dotnet exec ./tools/bin/Build/Build.dll "$@"
