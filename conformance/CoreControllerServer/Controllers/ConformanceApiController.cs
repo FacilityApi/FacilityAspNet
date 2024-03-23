52e6ebf7788a017790146618d88b5570c268c986
Ed Ball <ed.ball@faithlife.com>
2024-03-23 09:14:09 -0700

    Add special handling for Facility types in controllers.

--- conformance/CoreControllerServer/Controllers/ConformanceApiController.cs
+++ conformance/CoreControllerServer/Controllers/ConformanceApiController.cs
@@ -1,5 +1,6 @@
 using Facility.ConformanceApi;
 using Facility.ConformanceApi.Http;
+using Facility.Core;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreControllerServer.Controllers;
@@ -10,6 +11,22 @@ public partial class ConformanceApiController : Controller
 	{
 		m_api = api;
 	}
+
+	// should return 200 with {"id":1} (no name)
+	[HttpGet, Route("ServiceDto")]
+	public WidgetDto ServiceDto() => new() { Id = 1 };
+
+	// should return 200 with {"id":1} (no name)
+	[HttpGet, Route("ServiceResultSuccess")]
+	public ServiceResult<WidgetDto> ServiceResultSuccess() => ServiceResult.Success(new WidgetDto { Id = 1 });
+
+	// should return 413 with {"code":"RequestTooLarge","message":"The request is too large."}
+	[HttpGet, Route("ServiceResultFailure")]
+	public ServiceResult<WidgetDto> ServiceResultFailure() => ServiceResult.Failure(ServiceErrors.CreateRequestTooLarge());
+
+	// should return 413 with {"code":"RequestTooLarge","message":"The request is too large."}
+	[HttpGet, Route("ServiceErrorDto")]
+	public ServiceErrorDto ServiceErrorDto() => ServiceErrors.CreateRequestTooLarge();
 
 	private ConformanceApiHttpHandler GetServiceHttpHandler() => new(m_api);
 
--- src/Facility.AspNetCore/FacilityActionFilter.cs
+++ src/Facility.AspNetCore/FacilityActionFilter.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using Facility.Core;
+using Facility.Core.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -7,17 +10,32 @@ public sealed class FacilityActionFilter : ActionFilterAttribute
 {
 	public override void OnActionExecuting(ActionExecutingContext context)
 	{
+		// replace empty httpRequest argument with the actual request
 		if (context.ActionArguments.TryGetValue(c_httpRequestKey, out var request) && request?.GetType() == typeof(HttpRequestMessage))
 			context.ActionArguments[c_httpRequestKey] = FacilityAspNetCoreUtility.CreateHttpRequestMessage(context.HttpContext.Request);
 	}
 
 	public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
 	{
-		if (context.Result is ObjectResult { Value: HttpResponseMessage httpResponseMessage })
+		// special response handling for Facility types
+		if (context.Result is ObjectResult { Value: { } resultObject } && TryCreateHttpResponseMessage(resultObject) is { } httpResponseMessage)
 			await WriteAndDisposeHttpResponseMessage(httpResponseMessage, context);
 		else
 			await next();
 	}
+
+	private static HttpResponseMessage? TryCreateHttpResponseMessage(object resultObject) =>
+		resultObject switch
+		{
+			HttpResponseMessage message => message,
+			ServiceErrorDto error => FacilityAspNetCoreUtility.CreateHttpResponseMessage(error, s_httpContentSerializer),
+			ServiceResult { IsFailure: true } failure => FacilityAspNetCoreUtility.CreateHttpResponseMessage(failure.Error ?? new(), s_httpContentSerializer),
+			ServiceResult success => ServiceResultUtility.TryGetValue(success, out var value) && value is not null
+				? new HttpResponseMessage(HttpStatusCode.OK) { Content = s_httpContentSerializer.CreateHttpContent(value) }
+				: new HttpResponseMessage(HttpStatusCode.OK),
+			ServiceDto dto => new HttpResponseMessage(HttpStatusCode.OK) { Content = s_httpContentSerializer.CreateHttpContent(dto) },
+			_ => null,
+		};
 
 	private static async Task WriteAndDisposeHttpResponseMessage(HttpResponseMessage httpResponseMessage, ActionContext context)
 	{
@@ -28,6 +46,8 @@ public sealed class FacilityActionFilter : ActionFilterAttribute
 				await FacilityAspNetCoreUtility.WriteHttpResponseMessageAsync(httpResponseMessage, contextResponse);
 		}
 	}
+
+	private static readonly HttpContentSerializer s_httpContentSerializer = HttpContentSerializer.Create(SystemTextJsonServiceSerializer.Instance);
 
 	private const string c_httpRequestKey = "httpRequest";
 }
