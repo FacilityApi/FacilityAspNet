80b87054117bd948756421b95c88ca46ac76f954
Joseph Stewart <josephlikescode@gmail.com>
2026-04-24 16:00:30 -0700

    Apply suggestions from code review
    Co-authored-by: Ed Ball <ejball@gmail.com>

--- src/Facility.AspNetCore/FacilityRequestMessageFeature.cs
+++ src/Facility.AspNetCore/FacilityRequestMessageFeature.cs
@@ -1,12 +1,12 @@
 namespace Facility.AspNetCore;
 
 /// <summary>
-/// Exposes the HttpRequestMessage for a facility API request on the http context.
+/// Exposes the HttpRequestMessage for a Facility API request using HttpContext.Features.
 /// </summary>
 public sealed class FacilityRequestMessageFeature
 {
 	/// <summary>
-	/// The HttpRequestMessage used by facility for the API request.
+	/// The HttpRequestMessage used by Facility for the API request.
 	/// </summary>
 	public required HttpRequestMessage RequestMessage { get; init; }
 }
