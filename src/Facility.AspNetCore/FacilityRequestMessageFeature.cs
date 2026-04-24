c04a5ff198a29952849d99e78e26200c57819130
Joseph Stewart <joseph.stewart@logos.com>
2026-04-24 14:25:17 -0700

    Add summaries.

--- src/Facility.AspNetCore/FacilityRequestMessageFeature.cs
+++ src/Facility.AspNetCore/FacilityRequestMessageFeature.cs
@@ -1,6 +1,12 @@
 namespace Facility.AspNetCore;
 
+/// <summary>
+/// Exposes the HttpRequestMessage for a facility API request on the http context.
+/// </summary>
 public sealed class FacilityRequestMessageFeature
 {
+	/// <summary>
+	/// The HttpRequestMessage used by facility for the API request.
+	/// </summary>
 	public required HttpRequestMessage RequestMessage { get; init; }
 }
